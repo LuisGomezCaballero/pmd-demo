5192b3dacf2365f31f4ae37bc6c44747493b6677
GL553V <GL553V@192.168.0.155>
2018-01-26 12:24:20 +0100

    Project upload

---
+++ pmd-test/src/main/java/com/luisgomezcaballero/Main.java
@@ -0,0 +1,17 @@
+package com.luisgomezcaballero;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String myString1 = "Hello";
+		
+		if (myString1.equals(" World!")) {
+			System.out.println(myString1.concat(" Earth!"));
+		} else {
+			System.out.println(myString1.concat(" there!"));
+		}
+
+	}
+
+}
